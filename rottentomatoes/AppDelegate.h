edf1d6769a7001f719ccfcf6ac307f8d2e7d7721
Pradyut Panda <ppanda@zynga.com>
2013-10-19 17:19:19 -0700

    Initial Commit

---
+++ rottentomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottentomatoes
+//
+//  Created by ppanda on 10/19/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
